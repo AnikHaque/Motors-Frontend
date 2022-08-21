f7bc08d29c97b9bb413b163ed4288a1bf600e41e
AnikHaque <ekramul15-811@diu.edu.bd>
2022-08-21 11:51:39 -0500

    updated again

--- src/components/visit/Visit.js
+++ src/components/visit/Visit.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import IndividualProduct from '../individualproduct/IndividualProduct';
 import './Visit.css';
 
 const Visit = () => {
